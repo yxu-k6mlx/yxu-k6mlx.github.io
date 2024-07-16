ff9f1f05a2ec01ceb6b4511f9fdcac0100cbe04d
k6mlx <xuyanwen2xz@gmail.com>
2024-07-15 17:33:50 -0700

    Forget js, I'm using Python

---
+++ Projects/Wuziqi-Python/GUI/chessboard.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+import settings 
+
+class Chessboard(tk.Frame): 
+    pass 
+
+class Chessboard_Grid(tk.Frame): 
+    pass
---
+++ Projects/Wuziqi-Python/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk 
+import settings
+
+if __name__ == "__main__":
+    # PROGRAM ENTRY POINT
+    root = tk.Tk() 
+    root.configure(background=settings.ROOT_BG_COLOR)
+    
+
+    root.mainloop() # LOOP the program until EXIT or ERROR
