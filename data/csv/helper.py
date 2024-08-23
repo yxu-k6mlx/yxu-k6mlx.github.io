320122dea3e38f0e4576704bd24c9a15ac6a3f2d
k6mlx <xuyanwen2xz@gmail.com>
2024-08-22 22:53:34 -0700

    Got chinese morse codes in csv

---
+++ data/csv/helper.py
@@ -0,0 +1,11 @@
+import csv 
+import codecs
+line_num = 0
+with open(r'yxu-k6mlx.github.io\data\csv\twtelegraph.csv', mode='r') as mainlandCodes: 
+    cn_codes = csv.reader(mainlandCodes)
+    for lines in cn_codes: 
+        unicode_raw = lines[0]
+        string = bytes(unicode_raw, "ascii").decode("unicode-escape")
+        
+        print(string)
+        
