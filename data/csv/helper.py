8c85c990d95a451609eca7e9be3e00e72e2e5ff5
k6mlx <xuyanwen2xz@gmail.com>
2024-08-24 20:53:04 -0700

    helper

--- data/csv/helper.py
+++ data/csv/helper.py
@@ -5,7 +5,9 @@ with open(r'yxu-k6mlx.github.io\data\csv\twtelegraph.csv', mode='r') as mainland
     cn_codes = csv.reader(mainlandCodes)
     for lines in cn_codes: 
         unicode_raw = lines[0]
+        num_code = lines[1]
         string = bytes(unicode_raw, "ascii").decode("unicode-escape")
         
         print(string)
+        print(num_code)
         
